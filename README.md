7ffca2c7be2e4f8b60c6d191eb2d5132c99bbc1e
DavidDaoTu <tudv.mechatronics.hust@gmail.com>
2024-09-01 11:34:17 +0700

    Completed the modely.py

--- README.md
+++ README.md
@@ -25,3 +25,65 @@ cd ..
   * **2014 Test images [41K/6GB]** (extract the test2014 folder and place at location cocoapi/images/test2014/)
 
 4. The project is structured as a series of Jupyter notebooks that are designed to be completed in sequential order (`0_Dataset.ipynb, 1_Preliminaries.ipynb, 2_Training.ipynb, 3_Inference.ipynb`).
+
+--- PROJECT RUBRIC ---
+1. Files Submitted
+Criteria	
+Submission Files
+
+Submission Requirements
+The submission includes model.py and the following Jupyter notebooks, where all questions have been answered: 2_Training.ipynb, and 3_Inference.ipynb.
+
+2. model.py
+Criteria	
+CNNEncoder
+Submission Requirements:
+The chosen CNN architecture in the CNNEncoder class in model.py makes sense as an encoder for the image captioning task.
+
+RNNDecoder
+Submission Requirements:
+The chosen RNN architecture in the RNNDecoder class in model.py makes sense as a decoder for the image captioning task.
+
+3. Training.ipynb
+Criteria	Submission Requirements
+Using the Data Loader
+
+When using the get_loader function in data_loader.py to train the model, most arguments are left at their default values, as outlined in Step 1 of 1_Preliminaries.ipynb. In particular, the submission only (optionally) changes the values of the following arguments: transform, mode, batch_size, vocab_threshold, vocab_from_file.
+
+Step 1, Question 1
+
+The submission describes the chosen CNN-RNN architecture and details how the hyperparameters were selected.
+
+Step 1, Question 2
+
+The transform is congruent with the choice of CNN architecture. If the transform has been modified, the submission describes how the transform used to pre-process the training images was selected.
+
+Step 1, Question 3
+
+The submission describes how the trainable parameters were selected and has made a well-informed choice when deciding which parameters in the model should be trainable.
+
+Step 1, Question 4
+
+The submission describes how the optimizer was selected.
+
+Step 2
+
+The code cell in Step 2 details all code used to train the model from scratch. The output of the code cell shows exactly what is printed when running the code cell. If the submission has amended the code used for training the model, it is well-organized and includes comments.
+
+4. Inference.ipynb
+Criteria	Submission Requirements
+transform_test
+
+The transform used to pre-process the test images is congruent with the choice of CNN architecture. It is also consistent with the transform specified in transform_train in 2_Training.ipynb.
+
+Step 3
+
+The implementation of the sample method in the RNNDecoder class correctly leverages the RNN to generate predicted token indices.
+
+Step 4
+
+The clean_sentence function passes the test in Step 4. The sentence is reasonably clean, where any <start> and <end> tokens have been removed.
+
+Step 5
+
+The submission shows two image-caption pairs where the model performed well, and two image-caption pairs where the model did not perform well.
--- model.py
+++ model.py
@@ -22,11 +22,91 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1, batch_size=32):
+        super(DecoderRNN, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        
+        # Hyper-parameters: fine tuning later
+        self.num_layers = num_layers
+        self.drop_prob = 0.5 
+        self.batch_size = batch_size
+        
+        # Word embedding layer that turns a word into a vector with a specified size
+        self.embed = nn.Embedding(self.vocab_size, self.embed_size)
+        
+        # The LSTM layer takes embedded word vectors (of a specified size) as inputs
+        # and output the hidden states (short-term memory) with size of hidden_size
+        self.lstm = nn.LSTM(input_size=self.embed_size, 
+                            hidden_size=self.hidden_size,
+                            num_layers=self.num_layers)
+        
+        # init hidden layer weights
+        # self.hidden_cell_state = self.init_hidden(self.batch_size)
+        
+        ### Dropout layer (as suggested by https://arxiv.org/pdf/1411.4555)
+        self.dropout = nn.Dropout(self.drop_prob)
+        
+        ### The Linear layer that maps the hidden states (short-term memory) to
+        # the vocab_size as output
+        self.hidden2Vocab = nn.Linear(self.hidden_size, self.vocab_size)
+        
+        # init linear layer weights
+        self.init_linear()      
+        
         pass
     
+    def init_linear(self):
+        ''' Initialize weights for fully connected layer '''
+        initrange = 0.1
+        
+        # Set bias tensor to all zeros
+        self.hidden2Vocab.bias.data.fill_(0)
+        # FC weights as random uniform
+        self.hidden2Vocab.weight.data.uniform_(-1, 1)
+        
+    def init_hidden(self, batch_size):
+        ''' Initializes hidden state '''
+        # Create two new tensors with sizes n_layers x batch_size x n_hidden,
+        # Randomly initialization for hidden state and cell state of LSTM
+        hidden = (torch.rand(self.num_layers, batch_size, self.hidden_size),
+                    torch.rand(self.num_layers, batch_size, self.hidden_size))
+        return hidden
+        
     def forward(self, features, captions):
-        pass
+        """forward phase returns output"""
+        # Create embedded word vectors for captions [batch_size, seq_len]
+        #   + Extract captions without the last stop word: <end> 
+        #   + Create embedded word vectors with size [batch_size, seq_len, embed_size]
+        embeddings = self.embed(captions[:, :-1])  # Exclude the <end> token
+        
+        batch_size = captions.shape[0]
+        seq_len = captions.shape[1]
+        if self.batch_size != batch_size:
+            self.batch_size = batch_size
+            # init hidden layer weights
+            self.hidden_cell_state = self.init_hidden(self.batch_size)
+            
+        # Reshape the image feature vector from [batch_size, embed_size]
+        # to tensor size [batch_size, 1, embed_size]
+        features = features.view(self.batch_size, 1, -1)
+        
+        # Concat the image feature tensor to the first position of embedded_word_vectors
+        # reshape inputs to [seq_len, batch_size, embed_size]
+        inputs = torch.cat((features, embeddings), dim=1).view(seq_len, batch_size, -1) 
+        
+        ### Pass the inputs through the LSTM to get output
+        outputs, self.hidden_cell_state = self.lstm(inputs, self.hidden_cell_state)
+        # Reshape the outputs to [batch_size * seq_len, hidden_size] required by Project
+        outputs = outputs.view(-1, self.hidden_size)
+        
+        ### Pass the output to the Dropout layer
+        outputs = self.dropout(outputs)
+        
+        ### Pass the LSTM output through Linear to get the next word
+        outputs = self.hidden2Vocab(outputs)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
